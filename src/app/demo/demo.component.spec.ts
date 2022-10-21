d1e25928b513eb2cb49415846b3a3df47625761a
xiaotong guan <94739229+guanxiaotongyz@users.noreply.github.com>
2022-10-20 17:11:30 -0700

    Update demo.component.spec.ts
    test for page title

--- src/app/demo/demo.component.spec.ts
+++ src/app/demo/demo.component.spec.ts
@@ -65,3 +65,83 @@ describe("DemoComponent-pagetitle-defalutvalue", () => {
      expect(title1.textContent).toContain('ReadAlong Studio');
   });
 });
+
+// === test editable test page after edited ===
+describe("DemoComponent-pagetitle-edittvalue", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    component.setTitle("new editable Page");
+    fixture.detectChanges();
+
+  });
+
+  it("test editable page title after edit ", () => {
+    const { debugElement } = fixture;
+    const title1 = debugElement.query(By.css('#spanid')).nativeElement;
+    expect(title1.textContent).toContain('new editable Page');
+ });
+});
+
+
+// === test editable test page after edited empty value ===
+describe("DemoComponent-pagetitle-editempty", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    component.setTitle("");
+    fixture.detectChanges();
+
+  });
+
+  it("test editable page title after edit ", () => {
+    const { debugElement } = fixture;
+    const title1 = debugElement.query(By.css('#spanid')).nativeElement;
+    expect(title1.textContent).toBeNull;
+ });
+});
+
+// === test editable test page after twice edited ====
+describe("DemoComponent-pagetitle-edittwice", () => {
+  let component: DemoComponent;
+  let fixture: ComponentFixture<DemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MaterialModule],
+      declarations: [DemoComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+    
+    fixture = TestBed.createComponent(DemoComponent);
+    component = fixture.componentInstance;
+    component.setTitle("new editable Page");
+    component.setTitle("new editable Page 2");
+    fixture.detectChanges();
+
+  });
+
+  it("test editable page title after twice edit ", () => {
+    const { debugElement } = fixture;
+    const title1 = debugElement.query(By.css('#spanid')).nativeElement;
+    expect(title1.textContent).toContain('new editable Page 2');
+ });
+});
