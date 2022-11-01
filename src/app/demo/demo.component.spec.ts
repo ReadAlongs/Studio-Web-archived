7574ffb4c76cf4b1801b3a328100c69f0ebb2d59
yox chen <yoxchen@yoxdeMBP.lan>
2022-10-31 22:56:03 -0700

    Fix duplicated import

--- src/app/demo/demo.component.spec.ts
+++ src/app/demo/demo.component.spec.ts
@@ -3,8 +3,6 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 // add By to query
 import { By } from "@angular/platform-browser";
 
-import { By } from "@angular/platform-browser";
-
 import { MaterialModule } from "../material.module";
 import { DemoComponent } from "./demo.component";
 
